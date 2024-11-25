36e776590826df240ee48b643ad098577ef4ea4e
aniaania321 <s31154@pjwstk.edu.pl>
2024-11-25 13:28:28 +0100

    drugi przycisk

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 document.getElementById("button").addEventListener("click", function () {
     alert("jesteś super");
 });
+document.getElementById("button2").addEventListener("click", function () {
+    window.location.href = "https://gowish.com/s/drjy9u";
+});
